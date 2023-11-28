e3585fdcfa7075a278ee0cf8be63f9f7281a1589
nonk123 <me@nonk.dev>
2023-11-28 18:45:02 +0300

    Initial configuration (no deployment yet)

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+OUT=out
+mkdir -p "$OUT"
+
+function copy() {
+    file=$1
+    cp -r "$1" "$OUT"
+}
+
+function build() {
+    page=$1
+    j2 "$page" > "$OUT/$page"
+}
+
+sass index.scss assets/index.css
+
+copy assets
+copy docs
+
+build index.html
+build demos.html
+build hunk.html
