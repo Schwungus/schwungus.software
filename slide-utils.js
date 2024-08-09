e700c4f2a53581ce2855ec83fb1a7c0e068f4639
nonk123 <me@nonk.dev>
2024-08-09 23:25:58 +0300

    Apply insane animation techniques
    Also annihilate `Jenkinsfile` in the process...

---
+++ slide-utils.js
@@ -0,0 +1,39 @@
+(function () {
+    const slidesRoot = document.getElementById("slides");
+    const slides = slidesRoot.children;
+
+    let idx = 0;
+
+    for (const slide of slides) {
+        const nextButt = document.createElement("button");
+        nextButt.textContent = "Next";
+
+        const prevButt = document.createElement("button");
+        prevButt.textContent = "Previous";
+
+        // TODO: add event handlers.
+
+        const buttsRoot = document.createElement("div");
+
+        for (const node of [buttsRoot, nextButt, prevButt]) {
+            node.classList = ["slide-goto"];
+        }
+
+        nextButt.classList.add("next");
+        prevButt.classList.add("prev");
+
+        if (idx != 0) {
+            buttsRoot.appendChild(prevButt);
+        }
+
+        if (idx != slides.length - 1) {
+            buttsRoot.appendChild(nextButt);
+        }
+
+        slide.appendChild(buttsRoot);
+
+        idx++;
+    }
+
+    slidesRoot.focus({ preventScroll: true });
+})()
